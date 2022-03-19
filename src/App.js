e1fcc9a17ec15104a0c744f64063c28a02325553
HannahNguyen6729 <81440768+HannahNguyen6729@users.noreply.github.com>
2022-03-19 10:57:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import RenderCalculator from "./RenderCalculator/RenderCalculator";
+function App() {
+  return (
+    <div className="App">
+      <RenderCalculator/>
+    </div>
+  );
+}
+
+export default App;
